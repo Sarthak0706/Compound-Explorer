a270da5cc582184ff56dd24c9d3a85b634cb8e67
Sarthak Jain <sarthak4556@gmail.com>
2025-05-25 20:02:20 +0530

    readme file updated

--- backend/routes/compound.routes.js
+++ backend/routes/compound.routes.js
@@ -4,8 +4,6 @@ const compoundController = require('../controllers/compound.controller');
 
 router.get('/', compoundController.getAllCompounds);
 router.get('/:id', compoundController.getCompoundById);
-router.post('/', compoundController.createCompound);
 router.put('/:id', compoundController.updateCompound);
-router.delete('/:id', compoundController.deleteCompound);
 
 module.exports = router;
